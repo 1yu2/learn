38729240d885f905444a3acbe877aa4850da2953
1yu2 <1833905216@qq.com>
2025-08-29 12:50:36 +0800

    refactor: update stream execution comment
    - Added "ceshi" to the stream execution comment in the search_tool.py file.

--- chatbot_book/search_tool.py
+++ chatbot_book/search_tool.py
@@ -141,7 +141,7 @@ while True:
         print("👋 Goodbye!")
         break
 
-    # 流式执行
+    # 流式执行 ceshi
     events = graph.stream({"messages": [("user", user_input)]})
     final_assistant_message = None
 
